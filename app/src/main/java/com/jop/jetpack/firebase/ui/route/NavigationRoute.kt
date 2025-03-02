f2916a406fbad8e34e703648bd0db20b6830bcd7
Jevon Putra <jevonop88@gmail.com>
2025-03-03 06:45:19 +0700

    Add navigation Route

---
+++ app/src/main/java/com/jop/jetpack/firebase/ui/route/NavigationRoute.kt
@@ -0,0 +1,55 @@
+package com.jop.jetpack.firebase.ui.route
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import com.jop.jetpack.firebase.presentation.dashboard.view.DashboardScreen
+import com.jop.jetpack.firebase.presentation.location.view.LocationScreen
+import com.jop.jetpack.firebase.presentation.location.viewmodel.LocationViewModel
+import com.jop.jetpack.firebase.presentation.welcome.view.WelcomeScreen
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun NavigationRoute(modifier: Modifier, navController: NavHostController, showBottomBar: (Boolean) -> Unit) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = Route.SPLASH,
+        enterTransition = { scaleIn(tween(700), initialScale = 0.5f) + fadeIn(tween(50)) },
+        exitTransition = { scaleOut(tween(500), targetScale = 0.5f) + fadeOut(tween(50)) },
+        popEnterTransition = { scaleIn(tween(700), initialScale = 0.5f) + fadeIn(tween(50)) },
+        popExitTransition = { scaleOut(tween(500), targetScale = 0.5f) + fadeOut(tween(50)) }
+    ){
+        navigation(
+            startDestination = Route.HOME_DASHBOARD,
+            route = Route.HOME
+        ){
+            composable(route = Route.HOME_DASHBOARD){
+                showBottomBar(true)
+                DashboardScreen(navController = navController)
+            }
+
+            composable(route = Route.HOME_LOCATION){
+                showBottomBar(true)
+                val viewModel: LocationViewModel = koinViewModel()
+                val state by viewModel.state.collectAsState()
+                LocationScreen(navController = navController, state, viewModel::onEvent)
+            }
+        }
+
+        composable(route = Route.SPLASH){
+            showBottomBar(false)
+            WelcomeScreen(navController = navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/jop/jetpack/firebase/ui/route/Route.kt
@@ -0,0 +1,9 @@
+package com.jop.jetpack.firebase.ui.route
+
+object Route {
+    const val SPLASH = "splash"
+
+    const val HOME = "home"
+    const val HOME_DASHBOARD = "home/dashboard"
+    const val HOME_LOCATION = "home/location"
+}
